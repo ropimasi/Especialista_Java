6ce5d20507e73b897415a6d6e2850b1b565e3508
ropimasi <ropimasi@email.com>
2025-11-08 12:10:22 -0300

    Módulo 14 aula 09 concluída.

---
+++ src/dev/ropimasi/curso/especialistajava/modulo14/aula09_/financeira/modelo/Funcionario.java
@@ -0,0 +1,43 @@
+package dev.ropimasi.curso.especialistajava.modulo14.aula09_.financeira.modelo;
+
+public class Funcionario implements ClienteFinanciavel {
+
+	private static final int QUANTIDADE_SALARIO_LIMITE_CREDITO = 5;
+
+	private String nome;
+	private double salarioMensal;
+
+
+
+	public Funcionario(String nome, double salarioMensal) {
+		super();
+		this.nome = nome;
+		this.salarioMensal = salarioMensal;
+	}
+
+
+
+	public String getNome() {
+		return nome;
+	}
+
+
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+
+
+	public double getSalarioMensal() {
+		return salarioMensal;
+	}
+
+
+
+	@Override
+	public double calcularLimiteAprovado() {
+		return getSalarioMensal() * QUANTIDADE_SALARIO_LIMITE_CREDITO;
+	}
+
+}
