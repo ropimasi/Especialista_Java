1f5d8caf777abfc0f8cc721a6a0042759f96642f
ropimasi <ropimasi@email.com>
2025-12-27 17:10:21 -0300

    Módulo 16 aula 03 concluída.

---
+++ src/dev/ropimasi/curso/especialistajava/modulo16/aula03/Principal1.java
@@ -0,0 +1,39 @@
+package dev.ropimasi.curso.especialistajava.modulo16.aula03;
+
+import java.util.Scanner;
+import dev.ropimasi.curso.especialistajava.modulo16.aula03.estoque.Produto;
+
+
+
+
+public class Principal1 {
+	public static void main(String[] args) {
+
+		Produto produto = new Produto("Apple Watch");
+		produto.setAtivo();
+		produto.adicionarEstoque(20);
+
+		comprar(produto);
+	}
+
+
+
+	private static void comprar(Produto produto) {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Quantidade: ");
+		int quantidade = sc.nextInt();
+
+		efetuarBaixaEstoque(produto, quantidade);
+		System.out.println("Compra efetuada com sucesso!");
+		
+		sc.close();
+	}
+
+
+
+	private static void efetuarBaixaEstoque(Produto produto, int quantidade) {
+		produto.retirarEstoque(quantidade);
+		System.out.printf("%d unidades retiradas do estoque. Estoque atual: %d%n", quantidade,
+				produto.getQuantidadeEstoque());
+	}
+}
