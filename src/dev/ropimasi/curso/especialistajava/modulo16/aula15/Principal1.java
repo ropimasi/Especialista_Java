80dd4dbd0d20a81f06e64e6551a50c1b793caa97
ropimasi <ropimasi@email.com>
2026-01-20 13:45:22 -0300

    Módulo 16 aula 15 concluída.

---
+++ src/dev/ropimasi/curso/especialistajava/modulo16/aula15/Principal1.java
@@ -0,0 +1,58 @@
+package dev.ropimasi.curso.especialistajava.modulo16.aula15;
+
+import java.util.Scanner;
+import dev.ropimasi.curso.especialistajava.modulo16.aula15.estoque.BaixaEstoqueException;
+import dev.ropimasi.curso.especialistajava.modulo16.aula15.estoque.Produto;
+import dev.ropimasi.curso.especialistajava.modulo16.aula15.estoque.ProdutoException;
+
+
+
+
+public class Principal1 {
+	public static void main(String[] args) {
+
+		Produto produto = new Produto("Apple Watch");
+		//produto.setAtivo();
+		produto.adicionarEstoque(20);
+
+		comprar(produto);
+	}
+
+
+
+	private static void comprar(Produto produto) {
+		Scanner sc = new Scanner(System.in);
+
+		do {
+			try {
+				System.out.print("Quantidade: ");
+				int quantidade = sc.nextInt();
+
+				efetuarBaixaEstoque(produto, quantidade);
+				System.out.println("Compra efetuada com sucesso!");
+
+				break;
+			} catch (BaixaEstoqueException e) {
+				System.out.println("Erro na compra: " + e.getCause().getMessage());
+				//				e.printStackTrace();
+			}
+
+		} while (true);
+
+		sc.close();
+	}
+
+
+
+	private static void efetuarBaixaEstoque(Produto produto, int quantidade) throws BaixaEstoqueException {
+
+		try {
+			produto.retirarEstoque(quantidade);
+			System.out.printf("%d unidades retiradas do estoque. Estoque atual: %d%n", quantidade,
+					produto.getQuantidadeEstoque());
+		} catch (IllegalArgumentException | ProdutoException e) {
+			throw new BaixaEstoqueException("Erro ao realizar baixa no estoque.", e);
+		}
+
+	}
+}
---
+++ src/dev/ropimasi/curso/especialistajava/modulo16/aula15/estoque/BaixaEstoqueException.java
@@ -0,0 +1,9 @@
+package dev.ropimasi.curso.especialistajava.modulo16.aula15.estoque;
+
+public class BaixaEstoqueException extends Exception {
+
+	public BaixaEstoqueException(String message, Throwable cause) {
+		super(message, cause);
+	}
+
+}
