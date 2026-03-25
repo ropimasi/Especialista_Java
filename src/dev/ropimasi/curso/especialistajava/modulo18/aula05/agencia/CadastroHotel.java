322d529fd408f8aca252e54b6974e93232c9b17b
ropimasi <ropimasi@email.com>
2026-03-25 15:03:10 -0300

    Módulo 18 aula 05 concluída.

---
+++ src/dev/ropimasi/curso/especialistajava/modulo18/aula05/agencia/CadastroHotel.java
@@ -0,0 +1,17 @@
+package dev.ropimasi.curso.especialistajava.modulo18.aula05.agencia;
+
+import java.util.ArrayList;
+
+public class CadastroHotel {
+
+    private final ArrayList hoteis = new ArrayList();
+
+    public void adicionar(String nome, String cidade, double precoDiaria) {
+        hoteis.add(new Hotel(nome, cidade, precoDiaria));
+    }
+
+    public ArrayList obterTodos() {
+        return hoteis;
+    }
+
+}
