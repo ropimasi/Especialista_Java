5781d373938961311986c25cdab581233545fe35
ropimasi <ropimasi@email.com>
2026-08-19 09:41:21 -0300

    Módulo 18 aula 10 concluída.

---
+++ src/dev/ropimasi/curso/especialistajava/modulo18/aula10/agencia/HotelJaExistenteException.java
@@ -0,0 +1,9 @@
+package dev.ropimasi.curso.especialistajava.modulo18.aula10.agencia;
+
+public class HotelJaExistenteException extends RuntimeException{
+
+	public HotelJaExistenteException(String mensagem) {
+		super(mensagem);
+	}
+	
+}
---
+++ src/dev/ropimasi/curso/especialistajava/modulo18/aula10/agencia/HotelNaoEncontradoException.java
@@ -0,0 +1,9 @@
+package dev.ropimasi.curso.especialistajava.modulo18.aula10.agencia;
+
+public class HotelNaoEncontradoException extends RuntimeException{
+
+	public HotelNaoEncontradoException(String mensagem) {
+		super(mensagem);
+	}
+	
+}
