c6134d09b17a43308d913f78a4eb4247ba9fa540
ropimasi <ropimasi@email.com>
2026-08-28 13:02:15 -0300

    Módulo 18 aula 21 e 22 concluída.

---
+++ src/dev/ropimasi/curso/especialistajava/modulo18/aula21e22/Principal.java
@@ -0,0 +1,25 @@
+package dev.ropimasi.curso.especialistajava.modulo18.aula21e22;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class Principal {
+	
+	public static void main(String[] args) {
+		Set<Integer> numeros = new HashSet<>();
+		
+		numeros.add(100);
+		numeros.add(10);
+		numeros.add(200);
+		numeros.add(3);
+		System.out.println(numeros.add(5));
+		System.out.println(numeros.add(5));
+		System.out.println(numeros.add(null));
+		System.out.println(numeros.add(null));
+		
+		for (Integer numero : numeros) {
+			System.out.println(numero);
+		}
+	}
+
+}
