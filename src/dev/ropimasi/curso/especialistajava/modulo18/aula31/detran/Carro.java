1818d945b50a65047106795c448de49b4025f7b7
ropimasi <ropimasi@email.com>
2026-09-05 17:47:17 -0300

    Módulo 18 aula 31 concluída.

---
+++ src/dev/ropimasi/curso/especialistajava/modulo18/aula31/detran/Carro.java
@@ -0,0 +1,62 @@
+package dev.ropimasi.curso.especialistajava.modulo18.aula31.detran;
+
+import java.util.Objects;
+
+
+
+public class Carro implements Comparable<Carro> {
+
+	private final String placa;
+	private final String modelo;
+
+
+	public Carro(String placa, String modelo) {
+		super();
+		Objects.requireNonNull(placa, "Placa não pode ser nula");
+		Objects.requireNonNull(modelo, "Modelo não pode ser nulo");
+		this.placa = placa;
+		this.modelo = modelo;
+	}
+
+
+	public String getPlaca() {
+		return placa;
+	}
+
+
+	public String getModelo() {
+		return modelo;
+	}
+
+
+	@Override
+	public String toString() {
+		return "Carro [placa=" + placa + ", modelo=" + modelo + "]";
+	}
+
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(placa);
+	}
+
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Carro other = (Carro) obj;
+		return Objects.equals(placa, other.placa);
+	}
+
+
+	@Override
+	public int compareTo(Carro arg0) {
+		return this.placa.compareTo(arg0.placa);
+	}
+
+}
---
+++ src/dev/ropimasi/curso/especialistajava/modulo18/aula31/detran/Proprietario.java
@@ -0,0 +1,68 @@
+package dev.ropimasi.curso.especialistajava.modulo18.aula31.detran;
+
+import java.util.Objects;
+
+
+
+public class Proprietario {
+
+	private String nome;
+	private String cpf;
+
+
+	public Proprietario(String nome, String cpf) {
+		super();
+		Objects.requireNonNull(nome, "Nome não pode ser nulo");
+		Objects.requireNonNull(cpf, "CPF não pode ser nulo");
+		this.nome = nome;
+		this.cpf = cpf;
+	}
+
+
+	public String getNome() {
+		return nome;
+	}
+
+
+	public void setNome(String nome) {
+		Objects.requireNonNull(nome, "Nome não pode ser nulo");
+		this.nome = nome;
+	}
+
+
+	public String getCpf() {
+		return cpf;
+	}
+
+
+	public void setCpf(String cpf) {
+		Objects.requireNonNull(cpf, "CPF não pode ser nulo");
+		this.cpf = cpf;
+	}
+
+
+	@Override
+	public String toString() {
+		return "Proprietario [nome=" + nome + ", cpf=" + cpf + "]";
+	}
+
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(cpf);
+	}
+
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Proprietario other = (Proprietario) obj;
+		return Objects.equals(cpf, other.cpf);
+	}
+
+}
